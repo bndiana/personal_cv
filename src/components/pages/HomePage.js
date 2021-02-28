5b95cafdfff6c31a15c80ca972bd55de611fa75f
Diana Brihan <brihan.diana@yahoo.ro>
2021-02-28 22:24:10 +0200

    changes made for Netlify

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -14,24 +14,33 @@ export const HomePage = () => {
           </h1>
         </div>
         <div className="media-items-home-page">
-      
           <div className="home-icons">
             <a
               href="https://github.com/bndiana?tab=repositories"
               target="_blank"
+              rel="noopener noreferrer"
             >
               <i className="fab fa-github fa-3x"></i>
             </a>
             <a
               href="https://www.linkedin.com/in/diana-brihan-65617819b/"
               target="_blank"
+              rel="noopener noreferrer"
             >
               <i className="fab fa-linkedin fa-3x"></i>
             </a>
-            <a href="https://www.facebook.com/brihan.diana" target="_blank">
+            <a
+              href="https://www.facebook.com/brihan.diana"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="fab fa-facebook fa-3x"></i>
             </a>
-            <a href="https://www.instagram.com/brihandiana/" target="_blank">
+            <a
+              href="https://www.instagram.com/brihandiana/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="fab fa-instagram fa-3x"></i>
             </a>
           </div>
